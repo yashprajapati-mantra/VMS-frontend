cba6d6a9ea8188ca43632c2fc44ee6b9ce5f196f
harshpatel-mantra <harsh.patel@mantratec.com>
2025-09-24 10:56:30 +0530

    Add variable wise API routs

---
+++ src/api/apiConfig.js
@@ -0,0 +1,10 @@
+export const API_BASE_URL = 'http://192.168.8.235:5000/api';
+
+export const API_ROUTES = {
+    AUTH:{
+        LOGIN: `${API_BASE_URL}/users/login`,
+        FORGOT_PASSWORD: `${API_BASE_URL}/user/forgot-password`,
+        VERIFY_OTP: `${API_BASE_URL}/user/verify-otp`,
+        RESET_PASSWORD: `${API_BASE_URL}/user/reset-password`,
+    }
+}
